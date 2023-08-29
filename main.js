ce553718ac800c6627a63c00f2587b91a72e4bc5
Marian Murillo <marianmurillob123.88@gmail.com>
2023-08-28 20:25:33 -0600

    Agregando validaciones de espacios vacíos con mensajes de error o exito en inicio de sesión

--- main.js
+++ main.js
@@ -22,7 +22,10 @@ const expresiones = {
 }
 const loginForm=document.getElementById('login'); //formulario login
 const inputs= document.querySelectorAll("#login input"); // inputs del form login
-
+const campos={
+    correo:false,
+    password:false,
+}
 //validar al presionar teclas
 //${parámetro} para acceder a un parámetro
 //´#Variable i´ para concatenar cadenas
@@ -35,25 +38,33 @@ const validarFormulario=(e)=>{
                 document.querySelector('#grupo__correo i').classList.remove('fa-times-circle');
                 document.querySelector('#grupo__correo i').classList.add('fa-check-circle');
                 document.querySelector('#grupo__correo .formulario__input-error').classList.remove('formulario__input-error-activo');
+                campos.correo=true;
+                
             }else{
+             
                 document.querySelector('#grupo__correo i').classList.remove('fa-check-circle');
                 document.querySelector('#grupo__correo i').classList.add('fa-times-circle');
                 document.getElementById('grupo__correo').classList.add('formulario__grupo-incorrecto');
                 document.querySelector('#grupo__correo .formulario__input-error').classList.add('formulario__input-error-activo');
+                campos.correo=false;
             }
         break;
         case "password":
             if(expresiones.password.test(e.target.value)){
+                
                 document.getElementById('grupo__password').classList.remove('formulario__grupo-incorrecto');
                 document.getElementById('grupo__password').classList.add('formulario__grupo-correcto');
                 document.querySelector('#grupo__password i').classList.remove('fa-times-circle');
                 document.querySelector('#grupo__password i').classList.add('fa-check-circle');
                 document.querySelector('#grupo__password .formulario__input-error').classList.remove('formulario__input-error-activo');
+                campos.password=true;
+                
             }else{
                 document.querySelector('#grupo__password i').classList.remove('fa-check-circle');
                 document.querySelector('#grupo__password i').classList.add('fa-times-circle');
                 document.getElementById('grupo__password').classList.add('formulario__grupo-incorrecto');
                 document.querySelector('#grupo__password .formulario__input-error').classList.add('formulario__input-error-activo');
+                campos.password=false;
             }
         break;
    }
@@ -65,6 +76,21 @@ inputs.forEach((input)=>{
     input.addEventListener('blur', validarFormulario);
 });
 loginForm.addEventListener('submit',(e)=>{
+    document.getElementById('formulario__mensaje').classList.remove('formulario__mensaje-activo');
     e.preventDefault(); //no enviar los datos todavia
-
+    const terminos = document.getElementById('terminos');
+    if(campos.correo && campos.password && terminos.checked){
+       loginForm.reset();
+       document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+       setTimeout(()=>{
+        document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+       },4000);
+       //eliminar iconos 
+       document.querySelectorAll('.formulario__grupo-correcto').forEach((icono)=>{
+        icono.classList.remove('formulario__grupo-correcto');
+       });
+    } else{
+        document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+    }
+   
 });
